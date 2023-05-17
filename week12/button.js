529964aeb3c6306a63f2c084d9bc2401fd2658ee
Constantin <constantinzuza20@gmail.com>
2023-05-17 02:36:09 +0100

    non-repetitive attempt 1

--- week12/button.js
+++ week12/button.js
@@ -5,13 +5,36 @@ function loadFunction() {
 
 function addToList() {
     var ul = document.getElementsByTagName('ul')[0];
-
-    var random = Math.ceil(Math.random() * 100);
-
     var li = document.createElement('li');
+    var numbers = [];
+
+    var pickAgain = true;
+    while (pickAgain == true) {
+        var random = Math.ceil(Math.random() * 100);
+        var alreadyPicked = false;
+
+        for (var i = 0; i < numbers.length; i++) {
+            if (numbers[i] == random) {
+                alreadyPicked = true;
+            }
+
+            if (alreadyPicked == true) {
+                pickAgain = true;
+            }
+            else {
+                pickAgain = false;
+            }
+        }
+
+        numbers.push(random);
+    }
+
+
     var text = document.createTextNode(random);
     li.appendChild(text);
     ul.appendChild(li);
 }
+
+
 
 document.addEventListener('DOMContentLoaded', loadFunction);
